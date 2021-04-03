abc009f808b4da1e295664c2003dd80b18343c01
FinnWestern <fhafting@uwo.ca>
2021-04-03 12:07:47 -0300

    Create Ultrasonic.h

---
+++ MSEbot_basecode/Ultrasonic.h
@@ -0,0 +1,7 @@
+//functions specific for climber program
+
+int checkDistance(){	//check distance read by ultrasonic sensor in cm
+
+}
+
+//may need to put interrupts in here and put winch functions in motion.h
